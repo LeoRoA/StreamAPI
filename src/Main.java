ca0526a0015e13d952c8ba4efea3c4c406927150
LeoRoA <romanleontev@gmail.com>
2023-01-31 00:10:41 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+    }
+
+
+}
